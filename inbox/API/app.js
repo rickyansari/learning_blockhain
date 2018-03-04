6ca11b281e6a93a64b8787da9496c28cb3b9593d
rickyansari <mohammedashfaq.nitk@gmail.com>
2018-03-04 15:02:47 +0530

    1. Installed the express server.
    2. Deployed the contract on server initialization.
    3. moved the express code to API folder.

---
+++ inbox/API/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); //Gives constructor function used to get web3 instance.
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+const { interface , bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+deployContract =  async ()=>{
+  accounts =  await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments:["hi there"]})
+    .send({from: accounts[0], gas:'1000000'})
+}
+
+deployContract();
+
+
+app.get('/', (req, res) => {
+  console.log("First Account  Address", accounts[0]);
+  console.log("deployed contract", inbox);
+  res.send('Hello World!')
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
