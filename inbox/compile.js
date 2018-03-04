e2797add61b8e52b040effb712d2b56060520041
rickyansari <mohammedashfaq.nitk@gmail.com>
2018-03-04 11:05:40 +0530

    q~

---
+++ inbox/compile.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+/* reading sol file rather than importing it because in import statement node
+  expect a .js file but our file is .sol*/
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
