cc56349632d422704d1062f6bd4eadd6ad303cdb
rickyansari <mohammedashfaq.nitk@gmail.com>
2018-03-04 16:19:48 +0530

    Removed unwanted code

--- inbox/test/inbox.test.js
+++ inbox/test/inbox.test.js
@@ -1,63 +1,61 @@
-// const assert = require('assert');
-// const ganache = require('ganache-cli');
-// const Web3 = require('web3'); //Gives constructor function used to get web3 instance.
-// const provider = ganache.provider();
-// const web3 = new Web3(provider);
-// const { interface , bytecode } = require('../compile');
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); //Gives constructor function used to get web3 instance.
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+const { interface , bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async ()=>{
+  accounts =  await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments:["hi there"]})
+    .send({from: accounts[0], gas:'1000000'})
+})
+
+
+describe('deploy contract', ()=>{
+  it('fetch accounts details', ()=>{
+    assert.ok(inbox.options.address);
+  });
+
+  it('initialize with default value', async ()=>{
+    const messsage = await inbox.methods.message().call();
+    assert.equal(messsage, 'hi there');
+  });
+
+  it('update the message value', async ()=>{
+    const newMessage = "Bye Bye";
+    await inbox.methods.setMessage(newMessage).send({from: accounts[0]});
+    const messsage = await inbox.methods.message().call();
+    assert.equal(newMessage, messsage);
+  })
+})
+
+// class Car {
+//   drive() {
+//     return 'vroom';
+//   }
 //
-// let accounts;
-// let inbox;
+//   park() {
+//     return 'stopped';
+//   }
+// }
+// var car;
 //
-// beforeEach(async ()=>{
-//   accounts =  await web3.eth.getAccounts();
-//   inbox = await new web3.eth.Contract(JSON.parse(interface))
-//     .deploy({data: bytecode, arguments:["hi there"]})
-//     .send({from: accounts[0], gas:'1000000'})
+// beforeEach(()=>{
+//   car = new Car();
 // })
 //
-//
-// describe('deploy contract', ()=>{
-//   it('fetch accounts details', ()=>{
-//     assert.ok(inbox.options.address);
-//   });
-//
-//   it('initialize with default value', async ()=>{
-//     const messsage = await inbox.methods.message().call();
-//     assert.equal(messsage, 'hi there');
-//   });
-//
-//   it('update the message value', async ()=>{
-//     const newMessage = "Bye Bye";
-//     await inbox.methods.setMessage(newMessage).send({from: accounts[0]});
-//     const messsage = await inbox.methods.message().call();
-//     assert.equal(newMessage, messsage);
+// describe('Car', ()=>{
+//   it('can park', ()=> {
+//     // car = new Car();
+//     assert.equal(car.park(), 'stopped');
 //   })
 //
-//
+//   it('driving skills',()=>{
+//     assert.equal(car.drive(), 'vroom');
+//   })
 // })
-//
-// // class Car {
-// //   drive() {
-// //     return 'vroom';
-// //   }
-// //
-// //   park() {
-// //     return 'stopped';
-// //   }
-// // }
-// // var car;
-// //
-// // beforeEach(()=>{
-// //   car = new Car();
-// // })
-// //
-// // describe('Car', ()=>{
-// //   it('can park', ()=> {
-// //     // car = new Car();
-// //     assert.equal(car.park(), 'stopped');
-// //   })
-// //
-// //   it('driving skills',()=>{
-// //     assert.equal(car.drive(), 'vroom');
-// //   })
-// // })
--- loc/compile.js
+++ loc/compile.js
@@ -4,7 +4,7 @@ const solc = require('solc');
 
 /* reading sol file rather than importing it because in import statement node
   expect a .js file but our file is .sol*/
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const inboxPath = path.resolve(__dirname, 'contracts', 'LineOfCredit.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+module.exports = solc.compile(source, 1).contracts[':LineOfCredit'];
