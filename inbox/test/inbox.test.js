31b3783994d9fbd841afad0dbc2ad5a0e15c99f9
rickyansari <mohammedashfaq.nitk@gmail.com>
2018-03-04 13:21:52 +0530

    before dividing the provider method to Two lines,   deployed the contract to ganache network and got the instance of the contract

--- inbox/test/inbox.test.js
+++ inbox/test/inbox.test.js
@@ -1,17 +1,30 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); //Gives constructor function used to get web3 instance.
-const web3 = new Web3(ganache.provider());
 
+
+
+const web3 = new Web3(ganache.provider());
+const { interface , bytecode } = require('../compile');
+
+let accounts;
+let inbox;
 
 beforeEach(async ()=>{
   accounts =  await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments:["hi there"]})
+    .send({from: accounts[0], gas:'1000000'})
 })
 
 
 describe('deploy contract', ()=>{
   it('fetch accounts details', ()=>{
-    console.log("accounts\n", accounts);
+    assert.ok(inbox.options.address);
+  });
+
+  it('initialize with proper value', async ()=>{
+    inbox.op
   })
 
 })
