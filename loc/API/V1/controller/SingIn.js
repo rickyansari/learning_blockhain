a314c1973df0fbcbc1960a2232256569343490af
ashfaq <ashfaq.m@techsophy.com>
2018-03-08 11:48:57 +0530

    internal commit

--- loc/API/V1/controller/SingIn.js
+++ loc/API/V1/controller/SingIn.js
@@ -53,40 +53,40 @@ getUserDetails = (userName, contractsDetail)=>{
     })
 }
 
-getContractNameAndRoleOfUser = async (name, contract) => {
-  return Helper.getContractInstance(contract.address).then(async (response) => {
-    let contractStatus = await response.instance.methods.status().call();
-    console.log("contractStatus", contractStatus);
-    let success = false;
-    let role;
+getContractNameAndRoleOfUser = (name, contract) => {
+  return Helper.getContractInstance(contract.address).then((response) => {
+     response.instance.methods.getContractStatus().send().then((response)=>{
+      console.log("response", response);
+      let success = false;
+      let role;
 
-    if (contract.buyer && contract.buyer.name === name) {
-      role = "buyer";
-    } else if (contract.buyerBank && contract.buyerBank.name === name) {
-      role = "buyerBank";
-    } else if (contract.seller && contract.seller.name === name) {
-      if (!inValidStatusForSeller.includes(contractStatus)) {
-        role = "seller";
-      }
-    } else if (contract.sellerBank && contract.sellerBank.name === name) {
-      if (!inValidStatusForSellerBank.includes(contractStatus)) {
-        role = "sellerBank";
-      }
-    }
-    if (role) {
-      return {
-        success: true,
-        data: {
-          role: role,
-          contractName: contract.name
+      if (contract.buyer && contract.buyer.name === name) {
+        role = "buyer";
+      } else if (contract.buyerBank && contract.buyerBank.name === name) {
+        role = "buyerBank";
+      } else if (contract.seller && contract.seller.name === name) {
+        if (!inValidStatusForSeller.includes(contractStatus)) {
+          role = "seller";
         }
-      };
-    } else {
-      return { success: false };
-    }
-  });
+      } else if (contract.sellerBank && contract.sellerBank.name === name) {
+        if (!inValidStatusForSellerBank.includes(contractStatus)) {
+          role = "sellerBank";
+        }
+      }
+      if (role) {
+        return {
+          success: true,
+          data: {
+            role: role,
+            contractName: contract.name
+          }
+        };
+      } else {
+        return { success: false };
+      }
+    });
+  })
 };
-
 
 module.exports = {
   verifyUser: verifyUser,
